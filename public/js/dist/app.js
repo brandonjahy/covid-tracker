5fec0bdebaaed7f0ba8d9bd24fd2dff611e2128b
Brandonjahy <b.johnss97@gmail.com>
2020-12-10 15:07:45 -0800

    added extra api to display current covid data on load

--- public/js/dist/app.js
+++ public/js/dist/app.js
@@ -19,6 +19,35 @@ function initMap() {
 // API CALLS
 
 $('#container').ready(function() {
+    var request = $.ajax({
+        url: 'https://api.covidtracking.com/v1/us/current.json',
+        method: "GET",
+        
+      });
+       
+      request.done(function( data ) {
+
+        $('.TP').text(data[0].positive)
+        $('.TN').text(data[0].negative)
+        $('.CH').text(data[0].hospitalizedCurrently)
+        $('.death').text(data[0].death)
+    
+        console.log(data[0].positive);
+        console.log(data[0].negative);
+        console.log(data[0].hospitalizedCurrently);
+        console.log(data[0].death);
+      });
+       
+      request.fail(function( data ) {
+        console.log(data)
+      });
+    
+    })
+
+
+
+
+$('.select1').on("click",function() {
 var request = $.ajax({
     url: ' https://api.covidtracking.com/v1/states/info.json',
     method: "GET",
@@ -28,16 +57,9 @@ var request = $.ajax({
   request.done(function( data ) {
 
 
-    let name = data[0].name;
-    // data.forEach(function(value){
-    //     value = data[value]
-    //     console.log(value)
-    // });
-
     for(i = 0; i < data.length; i++) {
         var select = document.querySelector('.select1')
         select.options[select.options.length] = new Option(data[i].name)
-        console.log(data[i].name)
     }
 
     console.log(name)
